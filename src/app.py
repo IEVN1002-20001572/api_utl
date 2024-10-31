b650411409376560faa09da47c20d09d6a76aba3
Oscar Fernando Barajas Fuentes <20001572@alumnos.utleon.edu.mx>
2024-10-30 20:50:16 -0600

    api flask

--- src/app.py
+++ src/app.py
@@ -1,34 +1,87 @@
-from flask import Flask, request, jsonify
-from flask_mysqldb import MySQL
-
-
+from flask import Flask, request, jsonify  # type: ignore
+from flask_mysqldb import MySQL  # type: ignore
 
 from config import config
 
-app=Flask(__name__)
+app = Flask(__name__)
 
-con=MySQL(app)
+con = MySQL(app)
 
-@app.route("/alumnos",methods=['GET'])
+@app.route("/alumnos", methods=['GET'])
 def lista_alumnos():
     try:
-        cursor=con.connection.cursor()
-        sql='select * from alumnos'
+        cursor = con.connection.cursor()
+        sql = 'SELECT * FROM alumnos'
         cursor.execute(sql)
-        datos=cursor.fetchall()
-        alumnos=[]
+        datos = cursor.fetchall()
+        alumnos = []
         for fila in datos:
-            alumno={'matricula':fila[0],'nombre':fila[1],'apaterno':fila[2],'amaterno':fila[3],'correo':fila[4]}
+            alumno = {
+                'matricula': fila[0],
+                'nombre': fila[1],
+                'apaterno': fila[2],
+                'amaterno': fila[3],
+                'correo': fila[4]
+            }
             alumnos.append(alumno)
             print(alumnos)
-        return jsonify({'alumnos':alumnos,'mensaje':'Lista de alumnos','exito':True})
+        return jsonify({'alumnos': alumnos, 'mensaje': 'Lista de alumnos', 'exito': True})
     except Exception as ex:
-        return jsonify({"message": "Error al conectar a la base de datos {}".format(ex),'exito':False})
-    
-def pagina_no_encontrada(error):
-    return "<h1>La pagina que estas buscando no existe</h1>",400
+        return jsonify({"message": "Error al conectar a la base de datos {}".format(ex), 'exito': False})
 
-if __name__=="__main__":
+def leer_alumno_bd(matricula):
+    try:
+        cursor = con.connection.cursor()
+        sql = 'SELECT * FROM alumnos WHERE matricula = {0}'.format(matricula)
+        cursor.execute(sql)
+        datos = cursor.fetchone()
+        if datos is not None:
+            alumno = {
+                'matricula': datos[0],
+                'nombre': datos[1],
+                'apaterno': datos[2],
+                'amaterno': datos[3],
+                'correo': datos[4]
+            }
+            return alumno
+    except Exception as ex:
+        return jsonify({"message": "Error al conectar a la base de datos {}".format(ex)})
+
+@app.route("/alumnos/<mat>", methods=['GET'])
+def leer_alumno(mat):
+    try:
+        alumno = leer_alumno_bd(mat)
+        if alumno is not None:
+            return jsonify({'alumnos': alumno, 'mensaje': 'Alumno encontrado', 'exito': True})
+    except Exception as ex:
+        return jsonify({'mensaje': 'Alumno no encontrado', 'exito': False})
+
+@app.route("/alumnos", methods=['POST'])
+def registrar_alumno():
+    try:
+        alumno = leer_alumno_bd(request.json['matricula'])
+        if alumno is not None:
+            return jsonify({'message': 'Alumno ya existe', 'exito': False})
+        else:
+            cursor = con.connection.cursor()
+            sql = '''INSERT INTO alumnos (matricula, nombre, apaterno, amaterno, correo)
+                     VALUES ('{0}', '{1}', '{2}', '{3}', '{4}')'''.format(
+                     request.json['matricula'],
+                     request.json['nombre'],
+                     request.json['apaterno'],
+                     request.json['amaterno'],
+                     request.json['correo']
+            )
+            cursor.execute(sql)
+            con.connection.commit()
+            return jsonify({'mensaje': 'Alumno agregado', 'exito': True})
+    except Exception as ex:
+        return jsonify({"message": "Error al conectar a la base de datos {}".format(ex), 'exito': False})
+
+def pagina_no_encontrada(error):
+    return "<h1>La página que estás buscando no existe</h1>", 400
+
+if __name__ == "__main__":
     app.config.from_object(config['development'])
-    app.register_error_handler(404,pagina_no_encontrada)
+    app.register_error_handler(404, pagina_no_encontrada)
     app.run(host='0.0.0.0', port=5000)
